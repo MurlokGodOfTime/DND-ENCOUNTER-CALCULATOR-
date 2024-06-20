e6c2410ad4a1d484fb843dd06b4f014bd9aba900
MurlokGodOfTime <muriloalmeidaantunes@gmail.com>
2024-06-19 23:21:42 -0300

    Update Combination.cs

--- Calculadora/D&D Calculadora/Main/Combination.cs
+++ Calculadora/D&D Calculadora/Main/Combination.cs
@@ -2,19 +2,12 @@
 
 class Combination
 {
-
-    public int limite { get; private set; }
-
     internal void Main(int calcAnsw, int calcCap, int quant)
     {
         int[] crX = { 10, 25, 50, 100, 200, 450, 700, 1100, 1800, 2300, 2900, 3900, 5000, 5900, 7200, 8400, 10000,
                     11500, 13000, 15000, 18000, 20000, 22000, 25000, 33000, 41000, 50000, 62000, 75000, 90000, 105000, 120000, 135000, 155000 };
 
-        for (int i = 0; crX[i] < calcCap; i++)
-        {
-            limite = i;
-        }
-        List<List<int>> combinations = FindCombinationsInRange(crX, calcAnsw, calcCap, quant, limite);
+        List<List<int>> combinations = FindCombinationsInRange(crX, calcAnsw, calcCap, quant);
 
         Console.WriteLine($"Combinations of {quant} elements from array that sum between {calcAnsw} and {calcCap}:");
         foreach (var combination in combinations)
@@ -23,20 +16,20 @@ class Combination
         }
     }
 
-    static List<List<int>> FindCombinationsInRange(int[] crX, int minSum, int maxSum, int combinationLength, int limite)
+    static List<List<int>> FindCombinationsInRange(int[] crX, int minSum, int maxSum, int combinationLength)
     {
         List<List<int>> result = new List<List<int>>();
         List<int> currentCombination = new List<int>();
 
         Array.Sort(crX); // Sort array to optimize the combination finding process
 
-        FindCombinationsInRangeHelper(crX, minSum, maxSum, combinationLength, 0, currentCombination, result, limite);
+        FindCombinationsInRangeHelper(crX, minSum, maxSum, combinationLength, 0, currentCombination, result);
 
         return result;
     }
 
     static void FindCombinationsInRangeHelper(int[] crX, int minSum, int maxSum, int k, int start,
-                                              List<int> currentCombination, List<List<int>> result, int limite)
+                                              List<int> currentCombination, List<List<int>> result)
     {
         if (k == 0)
         {
@@ -47,10 +40,10 @@ class Combination
             }
             return;
         }
-        for (int i = start; i < limite; i++)
+        for (int i = start; crX[i] < maxSum; i++)
         {
             currentCombination.Add(crX[i]);
-            FindCombinationsInRangeHelper(crX, minSum, maxSum, k - 1, i, currentCombination, result, limite); // i - 1 é melhor ou pior?
+            FindCombinationsInRangeHelper(crX, minSum, maxSum, k - 1, i, currentCombination, result); // i - 1 é melhor ou pior?
             currentCombination.RemoveAt(currentCombination.Count - 1);
         }
     }
